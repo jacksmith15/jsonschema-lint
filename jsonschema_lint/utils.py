0c8b01ff52440a19091308ed1ea7845c32946f3d
Jack Smith <jack.je.smith@gmail.com>
2021-11-13 17:12:04 +0000

    Custom path glob matcher

---
+++ jsonschema_lint/utils.py
@@ -0,0 +1,54 @@
+import re
+from pathlib import Path
+
+
+def path_match(path: Path, glob: str) -> bool:
+    """Like Path.match, but with support for recursive patterns (**)."""
+    if path_pattern(glob).match(str(path)):
+        return True
+    return False
+
+def path_pattern(glob: str, sep: str = "/") -> re.Pattern:
+    absolute = glob.startswith(sep)
+    if absolute:
+        glob = glob[1:]
+    else:
+        if not glob.startswith("**/"):
+            glob = f"**/{glob}"
+
+    sep = re.escape(sep)
+
+    # Iteratively replace wildcards surrounded by directory separators,
+    # so that we can make those separators optional when next to a wildcard.
+    result = re.escape(glob).replace(
+        f"{sep}\\*\\*{sep}",
+        rf"({sep}(.+?{sep})?)?",
+    ).replace(
+        f"\\*\\*{sep}",
+        rf"({sep}?(.*?){sep})?",
+    ).replace(
+        f"{sep}\\*\\*",
+        rf"({sep}(.*?){sep}?)?",
+    ).replace(
+        "\\*\\*",
+        rf"({sep}?(.*?){sep}?)?",
+    ).replace(
+        f"{sep}\\*{sep}",
+        rf"{sep}?[^{sep}]+?{sep}?",
+    ).replace(
+        f"{sep}\\*",
+        rf"{sep}?[^{sep}]+?",
+    ).replace(
+        f"\\*{sep}",
+        rf"[^{sep}]+?{sep}?",
+    ).replace(
+        "\\*",
+        rf"[^{sep}]+?",
+    ) + "$"
+    # woof!
+
+    if absolute:
+        # Ensure absolute patterns only match absolute paths
+        result = f"{sep}{result}"
+
+    return re.compile("^" + result)
---
+++ tests/test_utils.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import pytest
+
+from jsonschema_lint.utils import path_pattern, path_match
+
+
+@pytest.mark.parametrize(
+    "pattern,path,expected",
+    [
+        ("file.json", "file.json", True),
+        ("file.json", "foo.json", False),
+        ("file.json", "dir/file.json", True),
+        ("file.json", "/dir/file.json", True),
+        ("*.json", "file.json", True),
+        ("*.json", "file.foo", False),
+        ("*.json", "dir/file.json", True),
+        ("*.json", "/dir/file.json", True),
+        ("**/*.json", "file.json", True),
+        ("**/*.json", "dir/file.json", True),
+        ("**/*.json", "/file.json", True),
+        ("**/*.json", "/dir/file.json", True),
+        ("/**/*.json", "file.json", False),
+        ("/**/*.json", "dir/file.json", False),
+        ("/**/*.json", "/file.json", True),
+        ("/**/*.json", "/dir/file.json", True),
+        ("**/dir/*.json", "dir/file.json", True),
+        ("**/dir/*.json", "/dir/file.json", True),
+        ("**/dir/*.json", "home/user/dir/file.json", True),
+        ("**/dir/*.json", "/home/user/dir/file.json", True),
+        ("/**/dir/*.json", "dir/file.json", False),
+        ("/**/dir/*.json", "/dir/file.json", True),
+        ("/**/dir/*.json", "home/user/dir/file.json", False),
+        ("/**/dir/*.json", "/home/user/dir/file.json", True),
+        ("dir/**/*.json", "dir/file.json", True),
+        ("dir/**/*.json", "/dir/file.json", True),
+        ("dir/**/*.json", "dir/subdir/file.json", True),
+        ("dir/**/*.json", "/dir/subdir/file.json", True),
+        ("/dir/**/*.json", "dir/file.json", False),
+        ("/dir/**/*.json", "/dir/file.json", True),
+        ("/dir/**/*.json", "dir/subdir/file.json", False),
+        ("/dir/**/*.json", "/dir/subdir/file.json", True),
+    ]
+)
+def test_path_match(pattern: str, path, expected: bool):
+    path = Path(path)
+    compiled_pattern = path_pattern(pattern)
+    assert path_match(path, pattern) is expected, f"""Failed:
+Expected: {expected}
+Pattern: {pattern}
+Path: {path}
+Compiled Pattern: {compiled_pattern}
+"""
